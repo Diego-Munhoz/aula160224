79608c619217292936077857429e50c7ded0e0ae
Diego-Munhoz <diemunhoz@gmail.com>
2024-02-16 12:25:44 +0000

    save1

--- app/src/main/java/primeiro_exemplo/Hello.java
+++ app/src/main/java/primeiro_exemplo/Hello.java
@@ -1,5 +1,7 @@
 package primeiro_exemplo;
 
+import primeiro_exemplo.modelos.Pessoa;
+
 public class Hello {
     public static void main(String[] args) {
         int cont = 10;
@@ -7,5 +9,27 @@ public class Hello {
         System.out.println("Olá Mundo!!!");
 
         System.out.println(cont + " : " + nome);
+
+        Pessoa p1 = new Pessoa();
+
+        p1.nome = "Diego";
+        p1.idade = 16;
+
+        System.out.println(p1.nome + " : " + p1.idade);
+
+        Pessoa p2 = p1;
+
+        p2.nome = "Munhoz";
+        
+        System.out.println(p2.nome);
+        System.out.println(p1.nome);
+
+        Pessoa p3 = new Pessoa();
+        p3.nome = "Clodoaldo";
+        p3.idade = 30;
+
+        System.out.println(p1.verificarMarioriedade());
+        System.out.println(p3.verificarMarioriedade());
+
     }
 }
